6c4f8993ffd0678b4d822056c6185e73d56a3228
hoangvantuyen <hoangvantuyen@gobiz.vn>
2025-02-06 09:24:02 +0700

    Update CI

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo 'Build image'
-docker build -f -t ghcr.io/nobitacnt/image-php-octane .
-docker tag ghcr.io/nobitacnt/image-php-octane ghcr.io/nobitacnt/image-php-octane
-docker push ghcr.io/nobitacnt/image-php-octane
